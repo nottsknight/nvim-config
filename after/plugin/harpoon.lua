d073f4ccf72927fbe8c1bcf227bb1f3e284b407a
Ian Knight <ian.knight2@nottingham.ac.uk>
2023-02-07 15:44:33 +0000

    Fix a gitignore issue

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,8 @@
+require('telescope').load_extension('harpoon')
+require('harpoon').setup()
+
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,20 @@
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set("n", "<leader>tt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>tf", vim.cmd.NvimTreeFocus)
